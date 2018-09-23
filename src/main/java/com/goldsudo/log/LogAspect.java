eb7327eae5de6b453ff0ae40133685edc8c8bfcd
goldsudo <goldsudo@gmail.com>
2018-09-23 23:17:44 +0800

    使用aop记录方法的执行过程

---
+++ src/main/java/com/goldsudo/log/LogAspect.java
@@ -0,0 +1,59 @@
+package com.goldsudo.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LogAspect {
+    private Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    @Pointcut("execution(public * com.goldsudo.web.*.*(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void deBefore(JoinPoint joinPoint) throws Throwable {
+        startTime.set(System.currentTimeMillis());
+        joinPoint.toString();
+        logger.info("方法执行开始,joinPoint:"+joinPoint);
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("方法的返回值 : " + ret);
+    }
+
+    //后置异常通知
+    @AfterThrowing("webLog()")
+    public void throwss(JoinPoint jp){
+        logger.error("方法异常时执行.....");
+    }
+
+    //后置最终通知,final增强，不管是抛出异常或者正常退出都会执行
+    @After("webLog()")
+    public void after(JoinPoint jp){
+        logger.info("方法执行结束,joinPoint:"+jp+"，执行耗时："+(System.currentTimeMillis()-startTime.get())+" ms.");
+    }
+
+}
